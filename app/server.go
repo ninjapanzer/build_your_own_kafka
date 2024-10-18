bafce1fa0baec1bf057e3d1cfbd6099d0bde0915
Paul Scarrone <paul@scarrone.co>
2024-10-17 21:06:50 -0400

    stuff

--- app/server.go
+++ app/server.go
@@ -2,15 +2,13 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
-
 func main() {
+	log.SetOutput(os.Stdout)
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
@@ -21,9 +19,23 @@ func main() {
 		fmt.Println("Failed to bind to port 9092")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	var c net.Conn
+	c, err = l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
+	}
+	b := make([]byte, 4)
+	_, err = c.Read(b)
+	if err != nil {
+		log.Fatal("Failed to Read from Connection: ", err.Error())
+	}
+	_, err = c.Write(b)
+	if err != nil {
+		log.Fatal("Failed to Write to Connection: ", err.Error())
+	}
+	err = c.Close()
+	if err != nil {
+		log.Fatal("closed ", err.Error())
 	}
 }
